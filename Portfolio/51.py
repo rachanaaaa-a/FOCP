7461926a8c8d456b21945f1c5d997512bcd19f97
rachanaaaa-a <160468018+rachanaaaa-a@users.noreply.github.com>
2025-01-16 20:56:31 +0545

    Delete Portfolio/51.py

--- Portfolio/51.py
+++
@@ -1,5 +0,0 @@
-#1Using command-line arguments involves the sys module. Review the docs for thismodule and using the information in there write a short program that when run
-#from the command-line reports what operating system platform is being used.
-import sys
-platform = sys.platform
-print(f"Operating system platform: {platform}")
